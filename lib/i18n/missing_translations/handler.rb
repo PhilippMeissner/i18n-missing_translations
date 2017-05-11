33d8c88d116a8d48e900d241127703963c297028
philippmeissner <pm@zeit.io>
2017-05-11 12:16:41 +0200

    :poop:

--- lib/i18n/missing_translations/handler.rb
+++ lib/i18n/missing_translations/handler.rb
@@ -2,7 +2,8 @@ module I18n
   class MissingTranslations
     module Handler
       def call(exception, locale, key, options)
-        I18n.missing_translations.log(exception.keys)
+        binding.pry
+        I18n.missing_translations.log(exception.keys) if MissingTranslation === exception
         super
       end
     end
